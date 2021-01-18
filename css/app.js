bf978d8fd675dfe0d76238f3aa9e91c0fc5e2ec9
Shubham Patel <75959964+shubhampatel01@users.noreply.github.com>
2021-01-19 01:10:48 +0530

    feat : Added card section

---
+++ css/app.js
@@ -0,0 +1,20 @@
+console.log('helow rold');
+
+
+$(document).ready(function () {
+    $("a").on('click', function (event) {
+
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
