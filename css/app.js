c5a5d4b71f931cdc91b062c09edcbe8dd858ae1b
Shubham Patel <75959964+shubhampatel01@users.noreply.github.com>
2021-04-01 08:30:36 +0530

    feat: added Scroll event

--- css/app.js
+++ css/app.js
@@ -1,20 +1,27 @@
-console.log('helow rold');
-
-
 $(document).ready(function () {
     $("a").on('click', function (event) {
-
         if (this.hash !== "") {
             event.preventDefault();
-
             var hash = this.hash;
-
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
             }, 800, function () {
-
                 window.location.hash = hash;
             });
-        } 
+        }
     });
 });
+function onMyClick() {
+    const hemburger = document.getElementById('hemburger');
+    const items = document.getElementById('menu-item');
+    const menuColor = document.getElementById('nav-container');
+    hemburger.classList.toggle('change');
+    menuColor.classList.toggle('change-bg');
+    items.classList.toggle('remove');
+
+}
+
+let body = document.body;
+body.addEventListener("onclick", () => {
+    console.log('recommended Desktop site');
+})
